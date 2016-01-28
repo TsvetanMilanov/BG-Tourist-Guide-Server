b7b90af77b52b4aeaeb6e44c3b7850d2c38fdb8c
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-01-28 18:13:42 +0200

    Added The skeleton of the server.

---
+++ Data/BGTouristGuide.Data/BGTouristGuideDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace BGTouristGuide.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Models;
+
+    public class BGTouristGuideDbContext : IdentityDbContext<User>
+    {
+#if (DEBUG)
+        public BGTouristGuideDbContext()
+             : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+#else
+        public BGTouristGuideDbContext()
+            : base("ProductionConnection", throwIfV1Schema: false)
+        {
+        }
+#endif
+
+        public static BGTouristGuideDbContext Create()
+        {
+            return new BGTouristGuideDbContext();
+        }
+    }
+}
