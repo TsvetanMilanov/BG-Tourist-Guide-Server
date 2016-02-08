c836203bca2227020c43fad96d6962c947ce5a1c
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-08 02:57:30 +0200

    Changed the generation of qr codes to be only for approved for visiting tourist sites.

--- Server/BGTouristGuide.Api/Controllers/QrCodesController.cs
+++ Server/BGTouristGuide.Api/Controllers/QrCodesController.cs
@@ -1,11 +1,13 @@
 ﻿namespace BGTouristGuide.Api.Controllers
 {
     using System.Linq;
+    using System.Web;
     using System.Web.Http;
 
+    using BGTouristGuide.Models;
     using Common.Constants;
     using Services.Contracts;
-    using System.Web;
+
     [RoutePrefix("api/QrCodes")]
     public class QrCodesController : ApiController
     {
@@ -23,6 +25,8 @@
         public IHttpActionResult GenerateQrCodesForAllTouristSites()
         {
             var urls = this.touristSites.GetAll()
+                .SelectMany(p => p.SubTouristSites)
+                .Where(t => t.Status == TouristSiteStatus.ApprovedForVisiting)
                 .ToList()
                 .Select(t => string.Format("{0}/api/TouristSites/Visit?id={1}", GlobalConstants.ProductionApplicationUrl, t.Id));
 
